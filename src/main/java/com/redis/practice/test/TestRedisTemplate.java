12058ff6e008c8ffa2742f8b70149ff290dc4f60
itoutsource.cz10@trinasolar.com <itoutsource.cz10@trinasolar.com>
2019-12-05 15:54:54 +0800

    基础信息

---
+++ src/main/java/com/redis/practice/test/TestRedisTemplate.java
@@ -0,0 +1,10 @@
+package com.redis.practice.test;
+
+
+
+public class TestRedisTemplate {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/redis/practice/utils/RedisUtil.java
@@ -0,0 +1,21 @@
+package com.redis.practice.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    public static RedisTemplate<String, Object> redisTemplate;
+
+    public Boolean setValue(String key, Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
